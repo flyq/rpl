88708d5422f6b4b6eeacb11468ba12d829519968
flyq <flyq951@gmail.com>
2019-06-09 02:33:31 -0700

    add ownership

---
+++ ownership_test/target/debug/deps/ownership_test-1b4f018b3d93cade.d
@@ -0,0 +1,5 @@
+/home/flyq/workspace/rust/rpl/ownership_test/target/debug/deps/ownership_test-1b4f018b3d93cade: src/main.rs
+
+/home/flyq/workspace/rust/rpl/ownership_test/target/debug/deps/ownership_test-1b4f018b3d93cade.d: src/main.rs
+
+src/main.rs:
---
+++ ownership_test/target/debug/ownership_test.d
@@ -0,0 +1,1 @@
+/home/flyq/workspace/rust/rpl/ownership_test/target/debug/ownership_test: /home/flyq/workspace/rust/rpl/ownership_test/src/main.rs
