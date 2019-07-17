4d014edda183018bcce2ca0270f0113d9b48d0db
flyq <flyq951@gmail.com>
2019-07-17 00:49:46 -0700

    add closure

---
+++ closure/target/debug/closure.d
@@ -0,0 +1,1 @@
+/home/flyq/workspaces/flyq/rpl/closure/target/debug/closure: /home/flyq/workspaces/flyq/rpl/closure/src/main.rs
---
+++ closure/target/debug/deps/closure-9badb93f98fcae0d.d
@@ -0,0 +1,5 @@
+/home/flyq/workspaces/flyq/rpl/closure/target/debug/deps/closure-9badb93f98fcae0d: src/main.rs
+
+/home/flyq/workspaces/flyq/rpl/closure/target/debug/deps/closure-9badb93f98fcae0d.d: src/main.rs
+
+src/main.rs:
